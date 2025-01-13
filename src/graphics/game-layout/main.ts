69c917f40f613c98fa09ab17e36e94b8151febcc
zoton2 <zoton2@gmail.com>
2025-01-13 09:51:15 +0000

    graphics: move 4x3 1p game layout to own file

--- src/graphics/game-layout/main.ts
+++ src/graphics/game-layout/main.ts
@@ -1,8 +1,19 @@
 import { createHead } from '@unhead/vue';
 import { createApp } from 'vue';
+import { createMemoryHistory, createRouter } from 'vue-router';
+import L_4x3_1p from './layouts/4x3-1p.vue';
 import App from './main.vue';
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [
+    { name: '4:3 1 Player', path: '/4x3-1p', component: L_4x3_1p },
+    { path: '/:pathMatch(.*)*', redirect: '/4x3-1p' },
+  ],
+});
 
 const app = createApp(App);
 const head = createHead();
 app.use(head);
+app.use(router);
 app.mount('#app');
--- src/graphics/game-layout/main.vue
+++ src/graphics/game-layout/main.vue
@@ -1,54 +1,12 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
-import ParticipantInfo from './components/ParticipantInfo.vue';
-import RunInfo from './components/RunInfo.vue';
-import TimerElem from './components/TimerElem.vue';
 
 useHead({ title: 'game-layout' });
 </script>
 
 <template>
   <div class="Background" />
-  <div class="Layout">
-    <RunInfo :class="$style.RunInfo" />
-    <TimerElem :class="$style.Timer" />
-    <!-- Camera Feed -->
-    <div class="Fixed Capture BorderRight" :class="$style.CameraFeed" />
-    <!-- Runner/Player -->
-    <ParticipantInfo :class="$style.Player" />
-    <!-- Host -->
-    <ParticipantInfo
-      :class="$style.Host"
-      header-colour="#0c3f6b"
-      header-text="Host"
-      header-font-size="14px"
-      header-width="66px"
-      name-font-size="20px"
-      cut-edge-size="8px"
-    />
-    <!-- Comm1 -->
-    <ParticipantInfo
-      :class="$style.Comm1"
-      header-colour="#055255"
-      header-text="Comm"
-      header-font-size="14px"
-      header-width="66px"
-      name-font-size="20px"
-      cut-edge-size="8px"
-    />
-    <!-- Comm2 -->
-    <ParticipantInfo
-      :class="$style.Comm2"
-      header-colour="#055255"
-      header-text="Comm"
-      header-font-size="14px"
-      header-width="66px"
-      name-font-size="20px"
-      cut-edge-size="8px"
-    />
-    <!-- Game Feed -->
-    <div class="Fixed Capture" :class="$style.GameFeed" />
-  </div>
+  <RouterView class="Layout" />
 </template>
 
 <style lang="css">
@@ -57,63 +15,4 @@ useHead({ title: 'game-layout' });
   https://github.com/vitejs/vite/issues/3924#issuecomment-1185919568
 */
 @import url('../_misc/common.css') layer(layer-1);
-</style>
-
-<style lang="scss" module>
-.RunInfo {
-  /* Putting some of the border outside visible space. */
-  left: -3px;
-  top: -3px;
-  width: 593px; /* 3px added because of above */
-  height: 165px; /* 3px added because of above */
-}
-
-.Timer {
-  left: 0px;
-  top: 162px;
-  width: 590px;
-  height: 97px;
-}
-
-.Player {
-  left: 0px;
-  top: 561px;
-  width: 590px;
-  height: 73px;
-}
-
-.Host {
-  left: 0px;
-  top: 631px;
-  width: 590px;
-  height: 46px;
-}
-
-.Comm1 {
-  left: 0px;
-  top: 674px;
-  width: 590px;
-  height: 46px;
-}
-
-.Comm2 {
-  left: 0px;
-  top: 717px;
-  width: 590px;
-  height: 46px;
-}
-
-.CameraFeed {
-  left: 0px;
-  top: 259px;
-  width: 590px;
-  height: 302px;
-}
-
-.GameFeed {
-  left: 590px;
-  top: 0px;
-  width: 1330px;
-  height: 1000px;
-}
 </style>
