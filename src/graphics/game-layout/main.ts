910c425bcfb66f973cb40bf5e6679e6ad38924fb
zoton2 <zoton2@gmail.com>
2025-01-13 15:36:33 +0000

    graphics: add start of game-layout 16:9 1 player

--- src/graphics/game-layout/main.ts
+++ src/graphics/game-layout/main.ts
@@ -2,12 +2,14 @@ import { createHead } from '@unhead/vue';
 import { createApp } from 'vue';
 import { createMemoryHistory, createRouter } from 'vue-router';
 import L_4x3_1p from './layouts/4x3-1p.vue';
+import L_4x3_2p from './layouts/4x3-2p.vue';
 import App from './main.vue';
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes: [
     { name: '4:3 1 Player', path: '/4x3-1p', component: L_4x3_1p },
+    { name: '16:9 1 Player', path: '/16x9-1p', component: L_16x9_1p },
     { path: '/:pathMatch(.*)*', redirect: '/4x3-1p' },
   ],
 });
