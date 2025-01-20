be77ebc94655081f889f63a74abbc52aed9066e6
zoton2 <zoton2@gmail.com>
2025-01-20 17:54:51 +0000

    extension: add DCA auto-fading on XR18 on OBS changes

--- src/extension/audio.ts
+++ src/extension/audio.ts
@@ -1,15 +1,22 @@
 import { difference } from 'lodash';
 import foobar2000 from './util/foobar2000.js';
+import { nodecg } from './util/nodecg.js';
 import obs, { type SceneTransitionStarted } from './util/obs.js';
+import xr18 from './util/xr18.js';
+
+const { faders } = nodecg.bundleConfig.xr18;
 
 obs.conn.on('SceneTransitionStarted', (d) => {
   const data = d as SceneTransitionStarted;
   // Only run this code is the scene is actually different.
   if (data.toScene !== data.fromScene) {
     // Parse the "parameters" from the scene names and create difference arrays.
-    // "Parameters" refers to "SCENE_NAME [PARAMETERS]", e.g. "Intermission [M]"
+    // "Parameters" refers to "SCENE_NAME [PARAMETERS]", e.g. "Intermission [M,!P,!G]"
     enum Params { // possible parameters
       MusicPlay = 'M', // music should be playing
+      NoPlayerMics = '!P', // *no* player microphones should be heard
+      NoReaderMics = '!R', // *no* reader microphones should be heard
+      NoGameAudio = '!G', // *no* "game" audio should be heard
     }
     const regex = () => /\[(.+)\]$/g;
     const toParams = regex().exec(data.toScene)?.[1].replace(/\s/g, '').split(',') ?? [];
@@ -25,6 +32,24 @@ obs.conn.on('SceneTransitionStarted', (d) => {
       foobar2000.play().catch(() => {});
     } else if (onlyInPrevious(Params.MusicPlay)) {
       foobar2000.pause().catch(() => {});
+    }
+    // Toggle "player" microphones.
+    if (onlyInCurrent(Params.NoPlayerMics)) {
+      xr18.fade(faders.player, 'down');
+    } else if (onlyInPrevious(Params.NoPlayerMics)) {
+      xr18.fade(faders.player, 'up');
+    }
+    // Toggle "reader" microphones.
+    if (onlyInCurrent(Params.NoReaderMics)) {
+      xr18.fade(faders.reader, 'down');
+    } else if (onlyInPrevious(Params.NoReaderMics)) {
+      xr18.fade(faders.reader, 'up');
+    }
+    // Toggle "game" audio.
+    if (onlyInCurrent(Params.NoGameAudio)) {
+      xr18.fade(faders.game, 'down');
+    } else if (onlyInPrevious(Params.NoGameAudio)) {
+      xr18.fade(faders.game, 'up');
     }
   }
 });
